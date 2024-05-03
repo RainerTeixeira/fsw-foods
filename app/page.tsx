93ade65603e2d87f3716b9df94fc6e77ee18cf7a
RainerTeixeira <raineroliveira94@hotmail.com>
2024-05-03 20:08:06 -0300

    chore: add initial prisma schema

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState } from "react";
+
+const About = () => {
+  const [name, setName] = useState();
+
+  return <h1> Rainer teixeira </h1>
+};
+
+export default About;
+ 
